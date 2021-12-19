031f3994373d3f764b6eebb485ae06a5dcccb92b
Slava Chuprov <v.e.chuprov@gmail.com>
2021-12-19 15:08:22 +0300

    Fixed tasks

--- src/tasks/Task6.java
+++ src/tasks/Task6.java
@@ -5,11 +5,9 @@ import common.Person;
 import common.Task;
 
 import java.time.Instant;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /*
 Имеются
@@ -20,21 +18,43 @@ import java.util.Set;
  */
 public class Task6 implements Task {
 
-  private Set<String> getPersonDescriptions(Collection<Person> persons,
-                                            Map<Integer, Set<Integer>> personAreaIds,
-                                            Collection<Area> areas) {
-    return new HashSet<>();
-  }
+    private Set<String> getPersonDescriptions(Collection<Person> persons,
+                                              Map<Integer, Set<Integer>> personAreaIds,
+                                              Collection<Area> areas) {
+/*
+10 минут чтобы написать этот вариант и 3 дня, чтобы сделать это через stream
+        Set<String> result = new HashSet<>();
+        for (Person person : persons) {
+            Set<Integer> areaIds = personAreaIds.get(person.getId());
+            for (int id : areaIds) {
+                for (Area area : areas) {
+                    if (area.getId() == id) {
+                        result.add(person.getFirstName() + " - " + area.getName());
+                    }
+                }
+            }
+        }
+        return result;
+*/
 
-  @Override
-  public boolean check() {
-    List<Person> persons = List.of(
-        new Person(1, "Oleg", Instant.now()),
-        new Person(2, "Vasya", Instant.now())
-    );
-    Map<Integer, Set<Integer>> personAreaIds = Map.of(1, Set.of(1, 2), 2, Set.of(2, 3));
-    List<Area> areas = List.of(new Area(1, "Moscow"), new Area(2, "Spb"), new Area(3, "Ivanovo"));
-    return getPersonDescriptions(persons, personAreaIds, areas)
-        .equals(Set.of("Oleg - Moscow", "Oleg - Spb", "Vasya - Spb", "Vasya - Ivanovo"));
-  }
+        return new HashSet<>(persons).stream()
+                .flatMap(person -> personAreaIds.get(person.getId()).stream()
+                        .flatMap(areaId -> areas.stream() //здесь по моей логике должен быть просто map, поскольку для каждого занчения areId мэппится одно значение string, но без flatmap не работает
+                                .filter(area -> area.getId().equals(areaId))
+                                .map(area -> person.getFirstName() + " - " + area.getName())))
+                .collect(Collectors.toSet());
+
+    }
+
+    @Override
+    public boolean check() {
+        List<Person> persons = List.of(
+                new Person(1, "Oleg", Instant.now()),
+                new Person(2, "Vasya", Instant.now())
+        );
+        Map<Integer, Set<Integer>> personAreaIds = Map.of(1, Set.of(1, 2), 2, Set.of(2, 3));
+        List<Area> areas = List.of(new Area(1, "Moscow"), new Area(2, "Spb"), new Area(3, "Ivanovo"));
+        return getPersonDescriptions(persons, personAreaIds, areas)
+                .equals(Set.of("Oleg - Moscow", "Oleg - Spb", "Vasya - Spb", "Vasya - Ivanovo"));
+    }
 }
